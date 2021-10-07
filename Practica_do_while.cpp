1553bc6940bb30d022e6bb320553a0bc838ecbc0
IscJlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-10-07 15:30:56 -0500

    Se agrego la practica del do while
    se trabajo con el equipo la practica de las tablas de multiplicar con do while

---
+++ Practica_do_while.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main(){
+    string user, pass, usuario = "Sistemas", password = "Admin01";
+    char seguir, opc;
+    int multiplica, numero, i,;
+    cout<<"Bienvenido por favor ingresa al sistema:\n";
+    cout<<"Ingresa tu usuario: ";
+    cin>>user;
+    cout<<"Ingresa tu password: ";
+    cin>>pass;
+    if(user == usuario && pass == password){
+    do{
+        cout<<"Por favor ingresa el numero de la tabla de multiplicar a genar: ";
+        cin>>numero;
+        for(int i = 1; i<= 10; i++ ){
+             multiplica = i * numero;
+             cout<<numero<<" x "<<i<<" = "<<multiplica<<endl;
+        }
+        cout<<"¿Quieres generar otra tabla de multiplicar(s/n)? ";
+        fflush(stdin);
+        cin>>seguir;
+    }
+    while(seguir!='n');
+   }
+   else{
+     cout<<"Usuario no valido por favor contacta a soporte"<<endl;
+   }
+    cin.get();
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Hello World Developers"<<endl;
-    return 0;
-    cin.get();
-}
